2d338379dbc8371d7350b880c22743a2b8912a98
nasnoush <98779137+nasnoush@users.noreply.github.com>
2024-10-08 21:23:01 +0200

    Derniere upload

---
+++ Libft_a_jour/libft/ft_substr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 20:18:48 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/08 20:18:48 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	len_s;
+
+	if (s == NULL)
+		return (NULL);
+	len_s = ft_strlen(s);
+	if (len > len_s - start)
+		len = len_s - start;
+	str = (char *)malloc(len + 1);
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
