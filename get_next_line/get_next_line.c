7a114caa4ce369269f7c54cb2ef7e5ec5268aac4
nasnoush <98779137+nasnoush@users.noreply.github.com>
2024-11-04 14:23:11 +0100

    Projet fini 4-11-2024

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,42 +6,38 @@
 /*   By: nadahman <nadahman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:32:53 by nadahman          #+#    #+#             */
-/*   Updated: 2024/10/30 14:21:23 by nadahman         ###   ########.fr       */
+/*   Updated: 2024/11/04 14:12:54 by nadahman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*ft_line(char **stock)
+char	*ft_free_stock(char *stock)
+{
+	free(stock);
+	stock = NULL;
+	return (stock);
+}
+
+static char	*ft_extract_line(char **stock)
 {
 	char	*line;
 	int		i;
 	char	*temp;
 
 	i = 0;
-	if (*stock == NULL)
+	if (*stock == NULL || **stock == '\0')
 		return (NULL);
 	while ((*stock)[i] != '\0' && (*stock)[i] != '\n')
 		i++;
 	line = (char *)malloc (i + 2);
 	if (line == NULL)
-	{
-		free(*stock);
-		*stock = NULL;
 		return (NULL);
-	}
 	strlcpy(line, *stock, i + 2);
 	if ((*stock)[i] == '\n')
 		temp = ft_strdup(*stock + i + 1);
 	else
-		temp = ft_strdup(*stock + i);
-	if (temp == NULL)
-	{
-		free(line);
-		free(*stock);
-		*stock = NULL;
-		return (NULL);
-	}
+		temp = NULL;
 	free(*stock);
 	*stock = temp;
 	return (line);
@@ -56,35 +52,22 @@ char	*get_next_line(int fd)
 	char		*new_stock;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-		return (NULL);
-	bytes_lu = read(fd, buffer, BUFFER_SIZE);
+		return (stock = ft_free_stock(stock));
+	bytes_lu = 1;
 	while (bytes_lu > 0)
 	{
+		bytes_lu = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_lu < 0)
+			return (stock = ft_free_stock(stock));
 		buffer[bytes_lu] = '\0';
 		new_stock = ft_strjoin(stock, buffer);
-		if (new_stock == NULL)
-		{
-			free(stock);
-			stock = NULL;
-			return (NULL);
-		}
 		stock = new_stock;
 		if (found_line(stock) != -1)
 			break ;
-		bytes_lu = read(fd, buffer, BUFFER_SIZE);
 	}
 	if (bytes_lu < 0 || (bytes_lu == 0 && (stock == NULL || *stock == '\0')))
-	{
-		free(stock);
-		stock = NULL;
-		return (NULL);
-	}
-	line = ft_line(&stock);
-	if (line == NULL)
-	{
-		free(stock);
-		stock = NULL;
-	}
+		return (stock = ft_free_stock(stock));
+	line = ft_extract_line(&stock);
 	return (line);
 }
 
@@ -110,5 +93,3 @@ int	main(void)
 	close(fd);
 	return (0);
 }*/
-
-
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: nadahman <nadahman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 10:05:28 by nadahman          #+#    #+#             */
-/*   Updated: 2024/10/30 13:35:30 by nadahman         ###   ########.fr       */
+/*   Updated: 2024/11/04 14:19:37 by nadahman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,12 @@ int	found_line(char *str)
 	return (-1);
 }
 
+char	*ft_free_join(char *s1)
+{
+	free(s1);
+	return (NULL);
+}
+
 char	*ft_strjoin(char *s1, char *s2)
 {
 	int		i;
@@ -48,32 +54,23 @@ char	*ft_strjoin(char *s1, char *s2)
 
 	i = 0;
 	j = 0;
-	/*if (s1 == NULL && s2 == NULL)
-		return (NULL);*/
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
 	if (s1 == NULL)
 		s1 = ft_strdup("");
 	if (s2 == NULL)
-	{
-		free(s1);
-		return (NULL);
-	}
-	concat = (char *)malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
+		return (s1 = ft_free_join(s1));
+	concat = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (concat == NULL)
-	{
-		free(s1);
-		return (NULL);
-	}
+		return (s1 = ft_free_join(s1));
 	while (s1[i])
 	{
 		concat[i] = s1[i];
 		i++;
 	}
 	while (s2[j])
-	{
-		concat[i + j] = s2[j];
-		j++;
-	}
-	concat[i + j] = '\0';
+		concat[i++] = s2[j++];
+	concat[i] = '\0';
 	free(s1);
 	return (concat);
 }
@@ -86,7 +83,7 @@ char	*ft_strdup(const char *str)
 	if (str == NULL)
 		return (NULL);
 	i = 0;
-	new = (char *)malloc((ft_strlen((char *)str)) + 1);
+	new = (char *)malloc(ft_strlen((char *)str) + 1);
 	if (new == NULL)
 		return (NULL);
 	while (str[i])
