04b55667b0d48fb7f75267a12af6e8ee9e582adf
nasnoush <98779137+nasnoush@users.noreply.github.com>
2025-02-10 14:20:51 +0100

    Add files via upload

--- philosopher/routine.c
+++ philosopher/routine.c
@@ -3,113 +3,159 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nas <nas@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: nadahman <nadahman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 12:38:34 by nadahman          #+#    #+#             */
-/*   Updated: 2025/02/09 18:36:52 by nas              ###   ########.fr       */
+/*   Updated: 2025/02/10 12:18:32 by nadahman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void philo_think(t_philosophe *philosophe)
+void	philo_think(t_philosophe *philosophe)
 {
-    int is_dead;
+	int	is_dead;
 
-    pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
-    is_dead = philosophe->philo->is_dead;
-    pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
-    if (is_dead)
-        return;
-    philosophe->etat = PENSER;
-    print_and_lock(philosophe, "is thinking\n");
-    usleep(500);
+	pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
+	is_dead = philosophe->philo->is_dead;
+	pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
+	if (is_dead)
+		return ;
+	philosophe->etat = PENSER;
+	print_and_lock(philosophe, "is thinking\n");
+	usleep(500);
 }
 
-void philo_eat(t_philosophe *philosophe)
+// void philo_eat(t_philosophe *philosophe)
+// {
+//     int is_dead;
+
+//     pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
+//     is_dead = philosophe->philo->is_dead;
+//     pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
+//     if (is_dead)
+//         return ;
+//      if (philosophe->philo->nbr_philo == 1)
+//     {
+//             pthread_mutex_lock(philosophe->forks_left);
+//             print_and_lock(philosophe, "has taken a fork\n");
+//             usleep(philosophe->philo->time_to_die * 1000);
+//             pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
+//             philosophe->philo->is_dead = 1;
+//             pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
+//             print_and_lock(philosophe, "died");
+//             pthread_mutex_unlock(philosophe->forks_left);
+//             return ;
+//      }
+//     pthread_mutex_lock(philosophe->forks_left);
+//     print_and_lock(philosophe, "has taken a fork\n");
+//     pthread_mutex_lock(philosophe->forks_right);
+//     print_and_lock(philosophe, "has taken a fork\n");
+
+//     pthread_mutex_lock(&philosophe->philo->meal_mutex);
+//     philosophe->last_meal_time = get_time(philosophe->philo);
+//     pthread_mutex_unlock(&philosophe->philo->meal_mutex);
+
+//     print_and_lock(philosophe, "is eating\n");
+
+//     pthread_mutex_lock(&philosophe->philo->nb_meal_mutex);
+//     philosophe->nb_meal++;
+//     pthread_mutex_unlock(&philosophe->philo->nb_meal_mutex);
+
+//     usleep(philosophe->philo->time_to_eat * 1000);
+
+//     pthread_mutex_unlock(philosophe->forks_right);
+//     pthread_mutex_unlock(philosophe->forks_left);
+// }
+
+void	philo_eat(t_philosophe *philosophe)
 {
-    int is_dead;
+	int	is_dead;
 
-    pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
-    is_dead = philosophe->philo->is_dead;
-    pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
-    if (is_dead)
-        return;
-     if (philosophe->philo->nbr_philo == 1)
-    {
-            pthread_mutex_lock(philosophe->forks_left);
-            print_and_lock(philosophe, "has taken a fork\n");
-            usleep(philosophe->philo->time_to_die * 1000);
-            pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
-            philosophe->philo->is_dead = 1;
-            pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
-            print_and_lock(philosophe, "died");
-            pthread_mutex_unlock(philosophe->forks_left);
-            return;
-     }
-    pthread_mutex_lock(philosophe->forks_left);
-    print_and_lock(philosophe, "has taken a fork\n");
-    pthread_mutex_lock(philosophe->forks_right);
-    print_and_lock(philosophe, "has taken a fork\n");
-
-    pthread_mutex_lock(&philosophe->philo->meal_mutex);
-    philosophe->last_meal_time = get_time(philosophe->philo);
-    pthread_mutex_unlock(&philosophe->philo->meal_mutex);
-
-    print_and_lock(philosophe, "is eating\n");
-
-    pthread_mutex_lock(&philosophe->philo->nb_meal_mutex);
-    philosophe->nb_meal++;
-    pthread_mutex_unlock(&philosophe->philo->nb_meal_mutex);
-
-    usleep(philosophe->philo->time_to_eat * 1000);
-
-    pthread_mutex_unlock(philosophe->forks_right);
-    pthread_mutex_unlock(philosophe->forks_left);
+	pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
+	is_dead = philosophe->philo->is_dead;
+	pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
+	if (is_dead)
+		return ;
+	if (philosophe->philo->nbr_philo == 1)
+	{
+		pthread_mutex_lock(philosophe->forks_left);
+		print_and_lock(philosophe, "has taken a fork\n");
+		usleep(philosophe->philo->time_to_die * 1000);
+		pthread_mutex_unlock(philosophe->forks_left);
+		pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
+		philosophe->philo->is_dead = 1;
+		pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
+		print_and_lock(philosophe, "died");
+		return ;
+	}
+	if (philosophe->id % 2 == 0)
+	{
+		pthread_mutex_lock(philosophe->forks_right);
+		print_and_lock(philosophe, "has taken a fork\n");
+		pthread_mutex_lock(philosophe->forks_left);
+	}
+	else
+	{
+		pthread_mutex_lock(philosophe->forks_left);
+		print_and_lock(philosophe, "has taken a fork\n");
+		pthread_mutex_lock(philosophe->forks_right);
+	}
+	print_and_lock(philosophe, "has taken a fork\n");
+	pthread_mutex_lock(&philosophe->philo->meal_mutex);
+	philosophe->last_meal_time = get_time(philosophe->philo);
+	pthread_mutex_unlock(&philosophe->philo->meal_mutex);
+	print_and_lock(philosophe, "is eating\n");
+	pthread_mutex_lock(&philosophe->philo->nb_meal_mutex);
+	philosophe->nb_meal++;
+	pthread_mutex_unlock(&philosophe->philo->nb_meal_mutex);
+	usleep(philosophe->philo->time_to_eat * 1000);
+	pthread_mutex_unlock(philosophe->forks_right);
+	pthread_mutex_unlock(philosophe->forks_left);
 }
 
-void philo_sleep(t_philosophe *philosophe)
+void	philo_sleep(t_philosophe *philosophe)
 {
-    int is_dead;
-    
-    pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
-    is_dead = philosophe->philo->is_dead;
-    pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
-    if (is_dead)
-        return;
-    philosophe->etat = DORMIR;
-    print_and_lock(philosophe, "is sleeping\n");
-    usleep(philosophe->philo->time_to_sleep * 1000);
+	int	is_dead;
+
+	pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
+	is_dead = philosophe->philo->is_dead;
+	pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
+	if (is_dead)
+		return ;
+	philosophe->etat = DORMIR;
+	print_and_lock(philosophe, "is sleeping\n");
+	usleep(philosophe->philo->time_to_sleep * 1000);
 }
 
-void *routine(void *arg)
+void	*routine(void *arg)
 {
-    t_philosophe *philosophe;
+	t_philosophe	*philosophe;
 
-    philosophe = (t_philosophe *)arg;
-    pthread_mutex_lock(&philosophe->philo->meal_mutex);
-    philosophe->last_meal_time = get_time(philosophe->philo);
-    pthread_mutex_unlock(&philosophe->philo->meal_mutex);
-    if (philosophe->id % 2 == 0)
-        usleep(philosophe->philo->time_to_eat * 500);
-    while (1)
-    {
-        pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
-        philosophe->philo->philo_dead = philosophe->philo->is_dead;
-        pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
-        if (philosophe->philo->philo_dead)
-            return NULL;
-        pthread_mutex_lock(&philosophe->philo->nb_meal_mutex);
-        if (philosophe->philo->nbr_of_times_each_philo_must_eat != -1 &&
-            philosophe->nb_meal >= philosophe->philo->nbr_of_times_each_philo_must_eat)
-        {
-            pthread_mutex_unlock(&philosophe->philo->nb_meal_mutex);
-            return NULL;
-        }
-        pthread_mutex_unlock(&philosophe->philo->nb_meal_mutex);
-        philo_think(philosophe);
-        philo_eat(philosophe);
-        philo_sleep(philosophe);
-    }
-    return NULL;
+	philosophe = (t_philosophe *)arg;
+	pthread_mutex_lock(&philosophe->philo->meal_mutex);
+	philosophe->last_meal_time = get_time(philosophe->philo);
+	pthread_mutex_unlock(&philosophe->philo->meal_mutex);
+	if (philosophe->id % 2 == 0)
+		usleep(philosophe->philo->time_to_eat * 500);
+	while (1)
+	{
+		pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
+		philosophe->philo->philo_dead = philosophe->philo->is_dead;
+		pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
+		if (philosophe->philo->philo_dead)
+			return (NULL);
+		pthread_mutex_lock(&philosophe->philo->nb_meal_mutex);
+		if (philosophe->philo->nbr_of_times_each_philo_must_eat != -1
+			&& philosophe->nb_meal >= philosophe->philo->nbr_of_times_each_philo_must_eat)
+		{
+			pthread_mutex_unlock(&philosophe->philo->nb_meal_mutex);
+			return (NULL);
+		}
+		pthread_mutex_unlock(&philosophe->philo->nb_meal_mutex);
+		philo_think(philosophe);
+		philo_eat(philosophe);
+		philo_sleep(philosophe);
+	}
+	return (NULL);
 }
--- philosopher/utils.c
+++ philosopher/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nas <nas@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: nadahman <nadahman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 11:04:21 by nas               #+#    #+#             */
-/*   Updated: 2025/02/09 16:06:21 by nas              ###   ########.fr       */
+/*   Updated: 2025/02/10 11:39:58 by nadahman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_atoi(char *str)
 	i = 0;
 	r = 0;
 	s = 1;
-	while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	while (str[i] == '-' || str[i] == '+')
 	{
@@ -39,33 +39,33 @@ int	ft_atoi(char *str)
 	return (r * s);
 }
 
-long long int get_time(t_philo *philo)
+long long int	get_time(t_philo *philo)
 {
 	struct timeval	cur;
-	long long int cur_time;
+	long long int	cur_time;
 
 	gettimeofday(&cur, NULL);
 	cur_time = (cur.tv_sec * 1000) + (cur.tv_usec / 1000);
-
 	return (cur_time - philo->start_time);
 }
 
-int is_dead(t_philosophe *philosophe)
+int	is_dead(t_philosophe *philosophe)
 {
-    long long int cur_time_meal;
-    
-    cur_time_meal = get_time(philosophe->philo);
-    if (cur_time_meal - philosophe->last_meal_time > philosophe->philo->time_to_die)
-    {
-        philosophe->etat = MORT;
-        return (1);
-    }
-    return (0);
+	long long int	cur_time_meal;
+
+	cur_time_meal = get_time(philosophe->philo);
+	if (cur_time_meal
+		- philosophe->last_meal_time > philosophe->philo->time_to_die)
+	{
+		philosophe->etat = MORT;
+		return (1);
+	}
+	return (0);
 }
 
-void destroy_mutex(t_philosophe *philosophe)
+void	destroy_mutex(t_philosophe *philosophe)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < philosophe->philo->nbr_philo)
@@ -77,23 +77,27 @@ void destroy_mutex(t_philosophe *philosophe)
 	pthread_mutex_destroy(&philosophe->philo->meal_mutex);
 	pthread_mutex_destroy(&philosophe->philo->is_dead_mutex);
 	pthread_mutex_destroy(&philosophe->philo->nb_meal_mutex);
-
 	free(philosophe->philo->forks);
-	
 }
 
-void print_and_lock(t_philosophe *philosophe, char *str)
+void	print_and_lock(t_philosophe *philosophe, char *str)
 {
-    int is_dead;
+	int	is_dead;
 
-    pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
-    is_dead = philosophe->philo->is_dead;
-    pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
+	pthread_mutex_lock(&philosophe->philo->is_dead_mutex);
+	is_dead = philosophe->philo->is_dead;
+	pthread_mutex_unlock(&philosophe->philo->is_dead_mutex);
+	if (!is_dead)
+	{
+		pthread_mutex_lock(&philosophe->philo->print_mutex);
+		printf("%lld %d %s", get_time(philosophe->philo), philosophe->id, str);
+		pthread_mutex_unlock(&philosophe->philo->print_mutex);
+	}
+}
 
-    if (!is_dead)
-    {
-        pthread_mutex_lock(&philosophe->philo->print_mutex);
-        printf("%lld %d %s", get_time(philosophe->philo), philosophe->id, str);
-        pthread_mutex_unlock(&philosophe->philo->print_mutex);
-    }
+void	free_all(t_philosophe *philosophe)
+{
+	free(philosophe->philo);
+	free(philosophe);
+	free(philosophe->philo->threads);
 }
